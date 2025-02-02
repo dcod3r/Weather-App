8dd9746c6aa3a9a5b0addca31c9e7054552f68f6
Pawan Kumar Gupta <pawankumarg6294@gmail.com>
2025-02-02 22:07:26 +0530

    Added File to Weather-App

---
+++ weather-app/src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+     <div className='searcg-bar'>
+        <input type="text" name="search" id="search" placeholder='Search' />
+        <img src={search_icon} alt={search_icon} />
+     </div>
+    </div>
+  )
+}
+
+export default Weather
