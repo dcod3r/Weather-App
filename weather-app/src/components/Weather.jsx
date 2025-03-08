db42b8a54e22b38861b902d5b87b970bf279a889
Pawan Kumar Gupta <pawankumarg6294@gmail.com>
2025-03-08 15:42:23 +0530

    Changes

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -5,7 +5,7 @@ import search_icon from '../assets/search.png'
 const Weather = () => {
   return (
     <div className='weather'>
-     <div className='searcg-bar'>
+     <div className='search-bar'>
         <input type="text" name="search" id="search" placeholder='Search' />
         <img src={search_icon} alt={search_icon} />
      </div>
